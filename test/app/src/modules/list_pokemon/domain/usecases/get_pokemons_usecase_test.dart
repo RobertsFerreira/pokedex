1c7346dd369193a69ddc687cf05446630eb43768
Robert Ferreira <robertsilva-ferreira@hotmail.com>
2023-09-23 17:28:15 -0300

    test(get pokemons test): added failure test for server failures

--- test/app/src/modules/list_pokemon/domain/usecases/get_pokemons_usecase_test.dart
+++ test/app/src/modules/list_pokemon/domain/usecases/get_pokemons_usecase_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:pokedex/app/src/core/errors/failure.dart';
 import 'package:pokedex/app/src/modules/list_pokemon/domain/repositories/pokemon_repository_interface.dart';
 import 'package:pokedex/app/src/modules/list_pokemon/domain/usecases/get_pokemons_usecase.dart';
 import 'package:pokedex/app/src/modules/list_pokemon/domain/usecases/interfaces/get_pokemons_interface.dart';
@@ -22,17 +23,44 @@ void main() {
   group(
     'Group of tests for get list of pokemons',
     () {
-      test(
-        'should return a list of pokemon entity from the repository',
-        () async {
-          when(() => repository.getListPokemons()).thenAnswer(
-            (_) async => Right(getListPokemonsMock),
+      group(
+        'Group of success tests',
+        () {
+          test(
+            'Should return a list of pokemon entity from the repository',
+            () async {
+              when(repository.getListPokemons).thenAnswer(
+                (_) async => Right(getListPokemonsMock),
+              );
+
+              final result = await getPokemonsUseCase();
+
+              verify(repository.getListPokemons).called(1);
+              expect(result, Right(getListPokemonsMock));
+            },
           );
+        },
+      );
 
-          final result = await getPokemonsUseCase();
+      group(
+        'Group of failures tests',
+        () {
+          test(
+            'Should return a ServerFailure when the server is not available',
+            () async {
+              final error = ServerFailure();
+              when(repository.getListPokemons).thenAnswer(
+                (_) async => Left(error),
+              );
+              final result = await getPokemonsUseCase();
 
-          verify(repository.getListPokemons);
-          expect(result, Right(getListPokemonsMock));
+              verify(repository.getListPokemons).called(1);
+              expect(
+                result,
+                Left(error),
+              );
+            },
+          );
         },
       );
     },
